79447f216753c8a87f1cad549589a7b575ce55bc
benjamindz <dzaferovicbenjamin@gmail.com>
2021-03-23 23:50:11 +0100

    Added ex06

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -6,8 +6,5 @@ function lastName(lastName) {
   return lastName.toLowerCase();
 };
 
-console.log(firstName("Benjamin"));
-console.log(lastName("DINA"));
-
 exports.firstName = firstName;
 exports.lastName = lastName;
